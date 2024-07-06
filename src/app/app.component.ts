17948dffbc4a02e7001edcbe0565b0eb44fb8c22
shakib(github) <mshassan.dhk313@gmail.com>
2024-07-06 23:00:47 +0600

    @ViewChild query with component reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { COURSES } from 'src/db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,10 @@ import { Course } from './model/course';
 })
 export class AppComponent {
   courses = COURSES;
- 
 
+  @ViewChild(CourseCardComponent) card: Component | undefined;
   onCardClicked(course: Course) {
-    console.log('card clicked', course);
+    console.log('card clicked', this.card);
   }
   browserEvent() {
     console.log('called');
