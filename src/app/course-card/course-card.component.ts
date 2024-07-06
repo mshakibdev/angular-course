2d190ac794b1e075f3393939380f3e4c3a7dffa2
shakib(github) <mshassan.dhk313@gmail.com>
2024-07-06 18:49:28 +0600

    core directive: *ngfor and it's property

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -2,17 +2,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Course } from '../model/course';
 
 @Component({
-  selector: 'app-course-card',
+  selector: 'course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css'],
 })
 export class CourseCardComponent {
   @Input({ required: true }) course: Course | undefined;
-
+  @Input() cardIndex: number | undefined;
   @Output() courseSelceted = new EventEmitter<Course>();
   onCourseViewed() {
     console.log('opk');
     this.courseSelceted.emit(this.course);
   }
- 
 }
